783165da7dbbf390fd0c79069451275c9c19168a
aalves <>
2024-06-26 01:51:10 -0300

    Ressignificando usuário para clientes e nova estrutura

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
+from datetime import date
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
-class User(db.Model):
-  __tablename__ = 'users'
+class ClientEnvVersion(db.Model):
+  __tablename__ = 'clients_version'
   id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
+  env = db.Column(db.Integer, nullable=False)
+  unimed = db.Column(db.Integer, unique=True, nullable=False)
+  url = db.Column(db.String(150), unique=True, nullable=False)
+  modify_date = db.Column(db.Date, nullable=False, default=date.today)
+  version_reg_date = db.Column(db.Date)
+  last_version = db.Column(db.String(20))
 
-  def __init__(self, username, email):
-    self.username = username
-    self.email = email
+  def __init__(self, env, unimed, url, modify_date, version_reg_date, last_version):
+    self.env = env
+    self.unimed = unimed
+    self.url = url
+    self.modify_date = modify_date
+    self.version_reg_date = version_reg_date
+    self.last_version = last_version
       
   def json(self):
-    return{'id': self.id, 'username': self.username,'email': self.email}
+    return{'id': self.id, 'env': self.env, 'unimed': self.unimed, 'url': self.url, 'modify_date': self.modify_date, 'version_reg_date': self.version_reg_date, 'last_version':self.last_version} 
 
 db.create_all()
 
@@ -25,65 +34,67 @@ db.create_all()
 def test():
   return make_response(jsonify({'message':'teste ok, subiu'}), 200)
 
-# Criar Usuário
-@app.route('/users',methods=['POST'])    
-def create_user():
+# Criar cliente
+@app.route('/client_version',methods=['POST'])    
+def create_client_env():
   try:
     data = request.get_json()
-    new_user = User(username=data['username'], email=data['email'])
-    db.session.add(new_user)
+    new_client_env = ClientEnvVersion(env=data['env'], unimed=data['unimed'], url=data['url'], modify_date=data['modify_date'], version_reg_date=data['version_reg_date'], last_version=['last_version'])
+    db.session.add(new_client_env)
     db.session.commit()
-    return make_response(jsonify({'message': 'usuario criado.'}), 201)
+    return make_response(jsonify({'message': 'Cliente / Ambiente criado.'}), 201)
   except e:
-    return make_response(jsonify({'message': 'Erro na criação de usuário'}), 500)
+    return make_response(jsonify({'message': 'Erro na criação do ambiente / cliente'}), 500)
 
-# Retornar todos os Usuários    
-@app.route('/users', methods=['GET'])
-def get_users():
+# Retornar todos os clientes    
+@app.route('/client_version', methods=['GET'])
+def get_clients():
   try:
-    users = User.query.all()
-    return make_response(jsonify([user.json() for user in users]), 200)
+    client_versions = ClientEnvVersion.query.all()
+    return make_response(jsonify([client_version.json() for client_version in client_versions ]), 200)
   except e:
-    return make_response(jsonify({'message': 'Erro na criação de usuário'}), 500)
+    return make_response(jsonify({'message': 'Erro ao retornar clientes'}), 500)
 
-# Retronar usuário pelo id
-@app.route('/users/<int:id>', methods=['GET'])
-def get_user(id):
+# Retronar cliente pelo id
+@app.route('/client_version/<int:id>', methods=['GET'])
+def get_client(id):
   try:
-    user = User.query.filter_by(id=id).first()
-    if user:
-      return make_response(jsonify({'user': user.json()}), 200)
-    return make_response(jsonify({'message': 'Usuário não encontrado'}), 404)
+    client_version = ClientEnvVersion.query.filter_by(id=id).first()
+    if client_version:
+      return make_response(jsonify({'client_version': client_version.json()}), 200)
+    return make_response(jsonify({'message': 'Cliente não encontrado'}), 404)
   except e:
-    return make_response(jsonify({'message': 'Erro ao recuperar o usuário'}),500)
+    return make_response(jsonify({'message': 'Erro ao recuperar o cliente'}),500)
 
-# Atualizar o usuário pelo id
-@app.route('/users/<int:id>', methods=['PUT'])
-def update_user (id):
+
+# Atualizar o cliente pelo id
+@app.route('/client_version/<int:id>', methods=['PUT'])
+def update_client (id):
   try:
-    user = User.query.filter_by(id=id).first()
-    if user:
+    client_version = ClientEnvVersion.query.filter_by(id=id).first()
+    if client_version:
       data = request.get_json()
-      user.username = data['username']
-      user.email = data['email']
+      client_version.env = data['env']
+      client_version.unimed = data['unimed']
+      client_version.url = data['url']
+      client_version.modify_date = data['modify_date']
+      client_version.version_reg_date = data['version_reg_date']
+      client_version.last_version = data['last_version']
       db.session.commit()
-      return make_response(jsonify({'message': 'Usuário atualizado'}), 200)
-    return make_response(jsonify({'message': 'Usuário não encontrado'}), 404)
+      return make_response(jsonify({'message': 'Cliente atualizado'}), 200)
+    return make_response(jsonify({'message': 'Cliente não encontrado'}), 404)
   except e:
-    return make_response(jsonify({'message': 'Erro ao atualizar o usuário'}), 500)
+    return make_response(jsonify({'message': 'Erro ao atualizar o cliente'}), 500)
   
-# Deletar o usuário
-@app.route ('/users/<int:id>', methods = ['DELETE'])
-def delete_user(id):
+# Deletar o cliente
+@app.route ('/client_version/<int:id>', methods = ['DELETE'])
+def delete_client(id):
   try:
-    user = User.query.filter_by(id=id).first()
-    if user:
-      db.session.delete(user)
+    client_version  = ClientEnvVersion.query.filter_by(id=id).first()
+    if client_version:
+      db.session.delete(client_version)
       db.session.commit()
-      return make_response(jsonify({'message': 'Usuário deletado'}), 200)
-    return make_response(jsonify({'message': 'Usuário não encontrado'}), 404)
+      return make_response(jsonify({'message': 'Cliente deletado'}), 200)
+    return make_response(jsonify({'message': 'Cliente não encontrado'}), 404)
   except e:
-    return make_response(jsonify({'message': 'Erro ao excluir o usuário'}), 500)
-
-
-
+    return make_response(jsonify({'message': 'Erro ao excluir o cliente'}), 500)
